0824c63d62a115702be68a33ef73ce5e7ae4b0dc
Jeff Holtzman <jkholtz@med.umich.edu>
2020-10-14 14:50:12 -0400

    trivial change to check end-to-end timing of deploy through AWS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi from Jeff!!! Really! Today too! And next week!
+          Hi from Jeff!!! Really! Today too! And next week! And a build!
         </p>
         <a
           className="App-link"
